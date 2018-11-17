5f981e2f22dc6b78a3ee368616c6a4748af63899
anyatibrian <anyatibrian@gmail.com>
2018-11-17 16:22:10 +0300

    adds  tables to the database  [finishes #162030799]

--- Api/models/database.py
+++ Api/models/database.py
@@ -26,4 +26,32 @@ class DBConnect:
         except(Exception, psycopg2.DatabaseError) as error:
             print(error)
 
-
+    def create_tables(self):
+        """function that tables in the database"""
+        query = (
+            """CREATE TABLE IF NOT EXISTS users(
+            user_id serial PRIMARY KEY NOT NULL ,
+            username VARCHAR(100) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            create_at VARCHAR(100) NOT NULL,
+            admin BOOLEAN DEFAULT TRUE 
+            )""",
+            """
+            CREATE TABLE IF NOT EXISTS parcel_orders(
+            parcel_id serial PRIMARY KEY NOT NULL,
+            name VARCHAR(100) NOT NULL,
+            description VARCHAR(100) NOT NULL,
+            destination VARCHAR(100) NOT NULL,
+            pickup VARCHAR(200) NOT NULL,
+            status VARCHAR(100) DEFAULT'pending',
+            current_location VARCHAR(100) NOT NULL,
+            delivery_price VARCHAR(100)NOT NULL,
+            created_at VARCHAR(100) NOT NULL,
+            user_id INT NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE 
+            )
+            """
+        )
+        # creating the tables
+        self.cursor.execute(query)
--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from Api import create_app
 import os
+from Api.models.database import DBConnect
+
+
 config_name = os.getenv("APP_SETTINGS")
 app = create_app(config_name)
 print(config_name)
 
 if __name__ == '__main__':
+    db = DBConnect(app.config['DATABASE_URL'])
     app.run()
