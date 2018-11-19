1d23590a4f8d016a48b77f7b7ab3423302b048f4
anyatibrian <anyatibrian@gmail.com>
2018-11-19 14:02:25 +0300

    adds endpoints for getting a specific product [finishes #162042536]

--- Api/api_v1/get_parcel_order_endpoints.py
+++ Api/api_v1/get_parcel_order_endpoints.py
@@ -1,9 +1,16 @@
 from Api.api_v1 import api_v1
 from flask import jsonify, request
-from flask_jwt_extended import get_jwt_identity,jwt_required
+from Api.models.parcel_orders import ParcelOrders
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
 @api_v1.route('/parcels', methods=['GET'])
 @jwt_required
 def get_parcel_orders():
     current_user = get_jwt_identity()
+    # accessing the parcels class
+    orders = ParcelOrders()
+    parcels = orders.get_users_parcel_orders(current_user['user_id'])
+    if parcels:
+        return jsonify({'parcel_orders': parcels})
+    return jsonify({'parcel_orders': 'your order is empty'})
