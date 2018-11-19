b6ad832c1784bce135a5e34c1f1b69cd82ce68c1
anyatibrian <anyatibrian@gmail.com>
2018-11-19 23:21:22 +0300

    adds the endpoint to edit parcel destinations [finishes #162036194]

--- Api/api_v1/put_parcel_order_endpoints.py
+++ Api/api_v1/put_parcel_order_endpoints.py
@@ -1,10 +1,13 @@
 from Api.api_v1 import api_v1
 from flask import request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from Api.models.parcel_orders import ParcelOrders
 
 
 @api_v1.route('/parcels/<int:parcelId>/destination', methods=['PUT'])
 @jwt_required
 def edit_parcel_destination(parcel_id):
     current_user = get_jwt_identity()
-    return jsonify({'user_info': current_user})
+    json_data = request.get_json(force=True)
+    parcels = ParcelOrders().update_parcel_destination(current_user['user_id'], parcel_id, json_data['destination'])
+    return jsonify({'user_info': parcels})
